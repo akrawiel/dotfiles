42e9242f9574e417680b63350b0ca5a0d6ba70e9
Aleksander Krawiel <a.krawiel@gmail.com>
2021-09-05 10:35:50 +0200

    Add initial Emacs configuration

---
+++ .emacs.d/init.el
@@ -0,0 +1,30 @@
+;; Packaging setup
+
+(setq package-enable-at-startup nil)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(package-initialize)
+
+(straight-use-package 'use-package)
+
+(setq straight-use-package-by-default t)
+
+;; Auto-load init.org file
+
+(use-package org
+  :config
+  (org-babel-load-file "~/.emacs.d/boot.org")
+  )
