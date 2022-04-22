c5c1ab5e2fe36c9e200af57bbc0b96c255ba5452
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2022-04-22 18:12:18 +0200

    Add i3-related config

---
+++ autostart.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
+export SSH_AUTH_SOCK
+
+export TERMINAL=alacritty
+
+pkill dunst &
+pkill numlockx &
+pkill flameshot &
+pkill redshift-gtk &
+pkill nitrogen &
+pkill nm-applet &
+pkill polkit-gnome-au &
+pkill xfce4-power-man &
+pkill solaar &
+pkill xfsettingsd &
+
+sleep 1
+
+setxkbmap pl -option caps:ctrl_modifier &
+xset s off &
+dunst &
+numlockx &
+flameshot &
+redshift-gtk &
+nm-applet &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+solaar -w hide &
+xfsettingsd --daemon &
+nitrogen --restore &
