6a77a8136bdba066310892490ea783283967587c
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2022-05-23 18:30:03 +0200

    Switch formatting servers order

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -45,10 +45,10 @@ return packer.startup(function()
           require("null-ls").builtins.code_actions.eslint_d,
           require("null-ls").builtins.diagnostics.eslint_d,
           require("null-ls").builtins.diagnostics.jsonlint,
-          require("null-ls").builtins.formatting.eslint_d.with {
+          require("null-ls").builtins.formatting.prettierd.with {
             to_temp_file = false
           },
-          require("null-ls").builtins.formatting.prettierd.with {
+          require("null-ls").builtins.formatting.eslint_d.with {
             to_temp_file = false
           },
         },
