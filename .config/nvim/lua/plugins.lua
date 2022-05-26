930a73f51e84b5c18d968f8283055cd69b1e7f58
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2022-05-26 10:36:22 +0200

    Add packer bootstrap

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,13 +1,27 @@
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]])
+local fn = vim.fn
 
-local packer = require 'packer'
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
-return packer.startup(function()
+if fn.empty(fn.glob(install_path)) > 0 then
+  print 'Downloading packer.nvim'
+
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+
+  print 'Restart neovim now'
+
+  vim.cmd [[2sleep]]
+
+  vim.cmd [[qa]]
+end
+
+return require('packer').startup(function()
+  vim.cmd([[
+    augroup packer_user_config
+      autocmd!
+      autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    augroup end
+  ]])
+
   -- base packer
   use { 'wbthomason/packer.nvim' }
 
@@ -166,4 +180,9 @@ return packer.startup(function()
 
   -- language highlighting
   use { 'ionide/Ionide-vim', run = 'make fsautocomplete' }
+
+  -- bootstrap sync check
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
