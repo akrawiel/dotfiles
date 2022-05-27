c6da5ce1e3d1496564ee3b88291d90386f1d3741
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2022-05-27 17:21:00 +0200

    Change default app booting logic

--- Scripts/boot-default-apps.sh
+++ Scripts/boot-default-apps.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
 
-firefox-developer-edition &
-slack &
-evolution &
-i3-msg 'workspace 1; exec alacritty' &
+[[ `ps ax -o pid,cmd | rg -e firefox-developer-edition/firefox$ | wc -l` -eq 0 ]] && firefox-developer-edition &
+[[ `ps ax -o pid,cmd | rg -e slack/slack | wc -l` -le 1 ]] && slack &
+[[ `ps ax -o pid,cmd | rg -e evolution$ | wc -l` -eq 0 ]] && evolution &
+[[ `ps ax -o pid,cmd | rg -e alacritty | wc -l` -le 1 ]] && i3-msg 'workspace 1; exec alacritty' &
+[[ `ps ax -o pid,cmd | rg -e ferdi/ferdi | wc -l` -le 1 ]] && ferdi &
