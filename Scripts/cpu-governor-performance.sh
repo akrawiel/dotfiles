d4dcc0d750624c654e21f36842f83ed9deab5fdd
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2022-06-24 15:07:52 +0200

    Add platform profile to CPU governor scripts

--- Scripts/cpu-governor-performance.sh
+++ Scripts/cpu-governor-performance.sh
@@ -2,3 +2,6 @@
 
 sudo cpupower frequency-set -g performance
 sudo cpupower frequency-set -u 3.9GHz
+sudo tee /sys/firmware/acpi/platform_profile > /dev/null << EOF
+performance
+EOF
--- Scripts/cpu-governor-powersave.sh
+++ Scripts/cpu-governor-powersave.sh
@@ -2,3 +2,6 @@
 
 sudo cpupower frequency-set -g powersave
 sudo cpupower frequency-set -u 1.6GHz
+sudo tee /sys/firmware/acpi/platform_profile > /dev/null << EOF
+balanced
+EOF
