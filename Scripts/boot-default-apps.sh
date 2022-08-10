723947113bf674779b4bf827b504cc54d095c440
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2022-08-10 15:52:07 +0200

    Remove alacritty booted by default

--- Scripts/boot-default-apps.sh
+++ Scripts/boot-default-apps.sh
@@ -3,5 +3,4 @@
 [[ `ps ax -o pid,cmd | rg -e firefox-developer-edition/firefox$ | wc -l` -eq 0 ]] && firefox-developer-edition &
 [[ `ps ax -o pid,cmd | rg -e slack/slack | wc -l` -le 1 ]] && slack &
 [[ `ps ax -o pid,cmd | rg -e evolution$ | wc -l` -eq 0 ]] && evolution &
-[[ `ps ax -o pid,cmd | rg -e alacritty | wc -l` -le 1 ]] && i3-msg 'workspace 1; exec alacritty' &
 [[ `ps ax -o pid,cmd | rg -e ferdi/ferdi | wc -l` -le 1 ]] && ferdi &
