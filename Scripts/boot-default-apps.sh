db11a926e355c5d945842f14fe93f9920fa2ecc9
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2022-09-07 18:16:19 +0200

    Remove smplayer from default apps

--- Scripts/boot-default-apps.sh
+++ Scripts/boot-default-apps.sh
@@ -4,4 +4,3 @@
 [[ `ps ax -o pid,cmd | rg -e slack/slack | wc -l` -le 1 ]] && slack &
 [[ `ps ax -o pid,cmd | rg -e evolution$ | wc -l` -eq 0 ]] && evolution &
 [[ `ps ax -o pid,cmd | rg -e ferdi/ferdi | wc -l` -le 1 ]] && ferdi &
-[[ `ps ax -o cmd | rg -e ^smplayer$ | wc -l` -eq 0 ]] && smplayer &
