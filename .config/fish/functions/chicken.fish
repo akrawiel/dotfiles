982d26730f6a51aa27a13438ed8391b38686930d
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-02-01 12:11:46 +0100

    You read that right

---
+++ .config/fish/functions/chicken.fish
@@ -0,0 +1,4 @@
+function chicken --wraps=mpv\ --loop\ \'https://www.youtube.com/watch\?v=msSc7Mv0QHY\' --description alias\ chicken\ mpv\ --loop\ \'https://www.youtube.com/watch\?v=msSc7Mv0QHY\'
+  mpv --loop 'https://www.youtube.com/watch?v=msSc7Mv0QHY' $argv
+        
+end
