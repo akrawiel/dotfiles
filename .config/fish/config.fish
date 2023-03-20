ed7fd74cdea31c7e450a27ef0f385bafa2f8f93c
Aleksander Krawiel <a.krawiel@gmail.com>
2023-03-20 23:03:44 +0100

    Add spicetify to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ fish_add_path $HOME/.yarn/bin
 fish_add_path $HOME/go/bin
 fish_add_path $HOME/Applications
 fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.spicetify
 fish_add_path /opt/applications
 fish_add_path /opt/android-sdk/platform-tools
 fish_add_path /opt/android-sdk/tools
