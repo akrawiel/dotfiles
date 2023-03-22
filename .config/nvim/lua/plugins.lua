cf4346c994abeead253d379888e4ad809fa1c352
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-03-22 20:54:47 +0100

    Move orgmode config to separate file

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -132,42 +132,7 @@ return require("packer").startup(function(use)
 	use({
 		"nvim-orgmode/orgmode",
 		config = function()
-			require("orgmode").setup({
-				mappings = {
-					prefix = "<space>o",
-					capture = {
-						org_capture_finalize = "S",
-						org_capture_refile = "R",
-						org_capture_kill = "Q",
-					},
-				},
-				win_split_mode = "float",
-				org_agenda_files = { "~/Dropbox/Documents/OrgSync/**/*" },
-				org_capture_templates = {
-					t = {
-						description = "Task",
-						template = "* TODO %?",
-						target = "~/Dropbox/Documents/OrgSync/Todo Daily.org",
-					},
-					l = { description = "Link", template = "* %?" },
-				},
-				org_default_notes_file = "~/Dropbox/Documents/OrgSync/Inbox.org",
-				org_priority_highest = "A",
-				org_priority_default = "B",
-				org_priority_lowest = "E",
-				org_tags_column = -64,
-				org_todo_keywords = { "TODO(t)", "NEXT(n)", "WAIT(w)", "|", "DONE(d)" },
-				org_todo_keyword_faces = {
-					TODO = ":foreground orange :weight bold",
-					NEXT = ":foreground white :weight bold",
-					WAIT = ":foreground red :weight bold",
-					DONE = ":foreground lightgreen :weight bold",
-				},
-				org_blank_before_new_entry = {
-					heading = false,
-					plain_list_item = false,
-				},
-			})
+      require("orgmode-config")()
 		end,
 	})
 	use({
