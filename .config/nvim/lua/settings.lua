1408e2758513fdc8b5997381941c036da9c0bb4a
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-03-22 20:54:33 +0100

    Change scheduled agenda highlight

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -3,7 +3,7 @@ vim.api.nvim_command [[:filetype plugin indent on]]
 
 vim.api.nvim_command [[:highlight StatusLineBold guifg=#313633 guibg=#ccdc90 gui=bold]]
 vim.api.nvim_command [[:highlight OrgAgendaScheduled guifg=#eeeeee gui=bold]]
-vim.api.nvim_command [[:highlight OrgAgendaScheduledPast guifg=#cccccc]]
+vim.api.nvim_command [[:highlight OrgAgendaScheduledPast guifg=black guibg=red gui=bold]]
 vim.api.nvim_command [[:highlight OrgAgendaDeadline guifg=#ff8888 gui=bold]]
 
 vim.opt.compatible = false
