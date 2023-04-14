f0bf1d87ce3a186c3bfa2e156ee4fefd61722c3a
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-04-14 08:27:05 +0200

    Add logseq to default apps

--- Scripts/boot-default-apps.fish
+++ Scripts/boot-default-apps.fish
@@ -3,4 +3,4 @@
 runSingleInstance 'firefox-developer-edition'
 runSingleInstance 'slack -s'
 runSingleInstance 'thunderbird-beta'
-runSingleInstance 'kitty --class TodoDaily -e /usr/bin/nvim "$HOME/Dropbox/Documents/OrgSync/Todo Daily.org"' 'TodoDaily'
+runSingleInstance 'logseq'
