3b3595a38764ff4b134e3a57a1bae8b1677fbee3
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-04-19 17:28:53 +0200

    Remove elm format formatting

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -74,7 +74,6 @@ return require("packer").startup(function(use)
 					require("null-ls").builtins.diagnostics.jsonlint,
 					require("null-ls").builtins.diagnostics.luacheck,
 					require("null-ls").builtins.diagnostics.yamllint,
-					require("null-ls").builtins.formatting.elm_format,
 					require("null-ls").builtins.formatting.prettierd,
 					require("null-ls").builtins.formatting.eslint_d,
 					require("null-ls").builtins.formatting.stylua,
