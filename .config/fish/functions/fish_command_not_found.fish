25766f2bdee3574e7c01634ef3c18a5b7401e6b0
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-05-04 20:19:09 +0200

    Add some more fish prompt customization

---
+++ .config/fish/functions/fish_command_not_found.fish
@@ -0,0 +1,3 @@
+function fish_command_not_found
+  echo -s (set_color red) "COMMAND NOT FOUND " (set_color normal) $argv
+end
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,24 +1,22 @@
 function fish_prompt
-  set -f git_branch_name (git rev-parse --abbrev-ref HEAD 2> /dev/null)
-
   set -f blue (set_color blue)
   set -f green (set_color green)
   set -f red (set_color red)
   set -f normal (set_color normal)
 
-  set -l cwd $blue(basename (prompt_pwd))
+  set -f git_branch_name (git rev-parse --abbrev-ref HEAD 2> /dev/null)
+  set -f cwd (basename (prompt_pwd))
+  set -f git_info ''
 
   if test -n "$git_branch_name"
-    set git_info $green(echo $git_branch_name)
-    set git_info " $git_info"
+    set git_info (echo -n -s ' ' $green $git_branch_name)
 
     set -f git_dirty (git status -s --ignore-submodules=dirty 2> /dev/null)
 
     if test -n "$git_dirty"
-      set -l dirty $red(echo "×")
-      set git_info "$git_info $dirty"
+      set git_info (echo -n -s $git_info ' ' $red '×')
     end
   end
 
-  echo -n -s $cwd $git_info $normal ' ' 
+  echo -n -s $blue $cwd $git_info $normal ' ' 
 end
