f449e4559510eba81f2cae619e86ab5bd7c10aaa
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-05-04 18:23:42 +0200

    Change default app booting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -76,8 +76,8 @@ bind --mode insert \cx _open_with_xdg
 set fish_greeting
 
 function fish_user_key_bindings
-    fish_default_key_bindings -M insert
-    fish_vi_key_bindings --no-erase insert
+  fish_default_key_bindings -M insert
+  fish_vi_key_bindings --no-erase insert
 end
 
 set -g fish_cursor_default block
--- Scripts/boot-default-apps.fish
+++ Scripts/boot-default-apps.fish
@@ -1,6 +1,7 @@
-#!/bin/fish
+#!/usr/bin/env fish
 
 runSingleInstance 'firefox'
 runSingleInstance 'slack -s'
+runSingleInstance '/var/lib/flatpak/exports/bin/com.slack.Slack' 'slack'
 runSingleInstance 'thunderbird'
-runSingleInstance 'logseq'
+runSingleInstance 'logseq' 'Logseq'
