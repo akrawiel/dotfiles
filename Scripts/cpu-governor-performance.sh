9cba4f779338c0e2810e617b4814aa0239dea4fc
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-05-09 10:58:48 +0200

    Change cpu governor scripts

--- Scripts/cpu-governor-performance.sh
+++ Scripts/cpu-governor-performance.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 sudo cpupower frequency-set -g performance
-sudo cpupower frequency-set -u 3.9GHz
-sudo tee /sys/firmware/acpi/platform_profile > /dev/null << EOF
-performance
-EOF
--- Scripts/cpu-governor-powersave.sh
+++ Scripts/cpu-governor-powersave.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 sudo cpupower frequency-set -g powersave
-sudo cpupower frequency-set -u 1.6GHz
-sudo tee /sys/firmware/acpi/platform_profile > /dev/null << EOF
-balanced
-EOF
