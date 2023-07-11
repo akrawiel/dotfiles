47fc509893ee1a37ad9680a260eec6cb2348c0a9
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-07-11 12:36:24 +0200

    Migrate old config files to plugins

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,102 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		require("treesitter-config")
+		require("orgmode").setup_ts_grammar()
+
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"css",
+				"dockerfile",
+				"elixir",
+				"fish",
+				"go",
+				"heex",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"org",
+				"php",
+				"prisma",
+				"python",
+				"regex",
+				"scss",
+				"svelte",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vue",
+				"yaml",
+			},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = { "org" },
+			},
+			indent = {
+				enable = true,
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "gnn",
+					node_incremental = "ai",
+					scope_incremental = "as",
+					node_decremental = "ad",
+				},
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["ab"] = "@block.outer",
+						["ib"] = "@block.inner",
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["aF"] = "@call.outer",
+						["iF"] = "@call.inner",
+						["ac"] = "@conditional.outer",
+						["ic"] = "@conditional.inner",
+						["aC"] = "@class.outer",
+						["iC"] = "@class.inner",
+						["aP"] = "@parameter.outer",
+						["iP"] = "@parameter.inner",
+					},
+				},
+			},
+		})
+
+		require("treesitter-context").setup({
+			enable = true,
+			max_lines = 0,
+			trim_scope = "outer",
+			patterns = {
+				default = {
+					"class",
+					"function",
+					"method",
+					"for",
+					"while",
+					"if",
+					"switch",
+					"case",
+				},
+				rust = {
+					"impl_item",
+					"struct",
+					"enum",
+				},
+				markdown = {
+					"section",
+				},
+				json = {
+					"pair",
+				},
+				yaml = {
+					"block_mapping_pair",
+				},
+			},
+		})
 	end,
 }
--- .config/nvim/lua/plugins/telescope_nvim.lua
+++ .config/nvim/lua/plugins/telescope_nvim.lua
@@ -1,6 +1,80 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	config = function()
-		require("telescope-config")
+		local actions = require("telescope.actions")
+		local action_state = require("telescope.actions.state")
+
+		require("telescope").setup({
+			defaults = {
+				prompt_prefix = " 💻 ",
+				entry_prefix = "    ",
+				selection_caret = " 👉 ",
+				multi_icon = " ✅ ",
+				disable_devicons = false,
+				color_devicons = true,
+
+				layout_config = {
+					height = 40,
+					prompt_position = "bottom",
+					mirror = true,
+				},
+				layout_strategy = "vertical",
+				results_title = false,
+				sorting_strategy = "descending",
+
+				mappings = {
+					i = {
+						["<C-n>"] = function() end,
+						["<C-p>"] = function() end,
+						["<C-t>"] = function() end,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-k>"] = actions.move_selection_previous,
+						["<tab>"] = actions.toggle_selection + actions.move_selection_next,
+						["<M-a>"] = actions.toggle_all,
+						["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+						["<C-h>"] = actions.which_key,
+						["<C-space>"] = function(prompt_bufnr, mode, target)
+							local picker = action_state.get_current_picker(prompt_bufnr)
+
+							actions.close(prompt_bufnr)
+
+							for _, entry in ipairs(picker:get_multi_selection()) do
+								pcall(vim.cmd, string.format("edit %s", entry.value))
+							end
+						end,
+						["<esc>"] = function(prompt_bufnr, mode, target)
+							actions.close(prompt_bufnr, mode, target)
+							vim.cmd([[stopinsert]])
+						end,
+						["<CR>"] = function(prompt_bufnr, mode, target)
+							actions.select_default(prompt_bufnr, mode, target)
+							vim.cmd([[stopinsert]])
+						end,
+					},
+				},
+			},
+
+			pickers = {
+				live_grep = {
+					layout_config = {
+						mirror = true,
+					},
+				},
+				find_files = {
+					previewer = false,
+				},
+			},
+
+			extensions = {
+				fzf = {
+					fuzzy = true,
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",
+				},
+			},
+		})
+
+		require("telescope").load_extension("fzf")
 	end,
 }
