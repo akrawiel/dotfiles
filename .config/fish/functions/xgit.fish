20c4478b435729ed02b8b707c278d607d71ee861
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-09-27 15:52:23 +0200

    Modify xgit function

--- .config/fish/functions/xgit.fish
+++ .config/fish/functions/xgit.fish
@@ -1,14 +1,17 @@
 function xgit
-  set -f option $argv[1]
-  echo $PWD
+  set option $argv[1]
+  set arg1 $argv[2]
 
   switch $option
     case delete-branches db
-      git branch --merged | rg -v '^\*|master' | xargs -r git branch -d
+      set value (default "$arg1" (git branch --show-current))
+      git branch --merged | rg -v "^\*|$value" | xargs -r git branch -d
     case prettier-diff prettier pd
-      git diff --name-only --diff-filter=ABCMRTUX origin/master | sed 's/^app\///' | rg -e '.(j|t)sx?$' | xargs yarn prettier --write
+      set value (default "$arg1" (git branch --show-current))
+      git diff --name-only --diff-filter=ABCMRTUX "origin/$value" | sed 's/^app\///' | rg -e '.(j|t)sx?$' | xargs yarn prettier --write
     case eslint-diff eslint ed
-      git diff --name-only --diff-filter=ABCMRTUX origin/master | sed 's/^app\///' | rg -e '.(j|t)sx?$' | xargs yarn eslint --fix
+      set value (default "$arg1" (git branch --show-current))
+      git diff --name-only --diff-filter=ABCMRTUX "origin/$value" | sed 's/^app\///' | rg -e '.(j|t)sx?$' | xargs yarn eslint --fix
     case "*"
       echo -s (set_color red) "COMMAND UNKNOWN " (set_color normal) $option
   end
