5ff3899b35cc36e66ab895cc9f5103609828993d
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-10-04 09:46:57 +0200

    Add flutter from bin dir

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ fish_add_path $HOME/go/bin
 fish_add_path $HOME/Applications
 fish_add_path $HOME/AppImages
 fish_add_path $HOME/Bin
+fish_add_path $HOME/Bin/flutter/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.spicetify
 fish_add_path /opt/applications
