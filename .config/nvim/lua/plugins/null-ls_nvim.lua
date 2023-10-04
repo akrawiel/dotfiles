7c5bc603cbaf88f6d34760e2b216d512f67ccb36
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-10-04 09:46:49 +0200

    Add dart formatter

--- .config/nvim/lua/plugins/null-ls_nvim.lua
+++ .config/nvim/lua/plugins/null-ls_nvim.lua
@@ -9,8 +9,9 @@ return {
 				require("null-ls").builtins.diagnostics.jsonlint,
 				require("null-ls").builtins.diagnostics.luacheck,
 				require("null-ls").builtins.diagnostics.yamllint,
-				require("null-ls").builtins.formatting.prettierd,
+				require("null-ls").builtins.formatting.dart_format,
 				require("null-ls").builtins.formatting.eslint_d,
+				require("null-ls").builtins.formatting.prettierd,
 				require("null-ls").builtins.formatting.stylua,
 			},
 		})
