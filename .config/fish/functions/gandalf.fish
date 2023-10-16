7446ff081c30199011b4649454e133d606956ccb
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-10-16 17:24:08 +0200

    Gandalf Gandalf

--- .config/fish/functions/gandalf.fish
+++ .config/fish/functions/gandalf.fish
@@ -1,4 +1,4 @@
-function gandalf --wraps=mpv\ --loop\ \'https://www.youtube.com/watch\?v=BBGEG21CGo0\' --description alias\ gandalf\ mpv\ --loop\ \'https://www.youtube.com/watch\?v=BBGEG21CGo0\'
-  mpv --loop 'https://www.youtube.com/watch?v=BBGEG21CGo0' $argv
-        
+function gandalf --wraps=mpv\ --loop\ \'https://www.youtube.com/watch\?v=BBGEG21CGo0\' --description gandalf
+  mpv --loop --ytdl-format="bestvideo[height<=720][fps<=30]+bestaudio" 'https://www.youtube.com/watch?v=BBGEG21CGo0' $argv &;
+  disown;
 end
