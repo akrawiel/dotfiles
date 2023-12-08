acb41a5f25a1d2bb133dde2de0b38e5f48aa878f
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-12-08 18:19:13 +0100

    Remove all null ls formatting

--- .config/nvim/lua/plugins/null-ls_nvim.lua
+++ .config/nvim/lua/plugins/null-ls_nvim.lua
@@ -3,16 +3,14 @@ return {
 	config = function()
 		require("null-ls").setup({
 			sources = {
-				require("null-ls").builtins.code_actions.eslint_d,
-				require("null-ls").builtins.diagnostics.eslint_d,
+				-- require("null-ls").builtins.code_actions.eslint_d,
+				require("null-ls").builtins.code_actions.xo,
+				-- require("null-ls").builtins.diagnostics.eslint_d,
 				require("null-ls").builtins.diagnostics.fish,
 				require("null-ls").builtins.diagnostics.jsonlint,
 				require("null-ls").builtins.diagnostics.luacheck,
 				require("null-ls").builtins.diagnostics.yamllint,
-				require("null-ls").builtins.formatting.dart_format,
-				require("null-ls").builtins.formatting.eslint_d,
-				require("null-ls").builtins.formatting.prettierd,
-				require("null-ls").builtins.formatting.stylua,
+				require("null-ls").builtins.diagnostics.xo,
 			},
 		})
 	end,
