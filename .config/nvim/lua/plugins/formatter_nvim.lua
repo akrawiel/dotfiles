3134aa298de669660be359cd5e21dc40a67d5617
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2024-01-11 17:26:19 +0100

    Stop with the commented out stuff

--- .config/nvim/lua/plugins/formatter_nvim.lua
+++ .config/nvim/lua/plugins/formatter_nvim.lua
@@ -18,23 +18,45 @@ return {
 			}
 		end
 
-    local xo_setup = { xo }
-    local eslintd_prettierd_setup = { require("formatter.filetypes.javascript").eslint_d, require("formatter.filetypes.javascript").prettierd }
+    local eslintd_prettierd_setup = {
+			require("formatter.filetypes.javascript").eslint_d,
+			require("formatter.filetypes.javascript").prettierd
+		}
+
+		local js_setup = {}
+
+		if vim.fn.findfile(".xo-config.json") ~= nil then
+			table.insert(js_setup, xo)
+		elseif vim.fn.findfile(".eslintrc.js") ~= nil
+			or vim.fn.findfile(".eslintrc.json") ~= nil
+			or vim.fn.findfile(".eslintrc") ~= nil
+		then
+			table.insert(
+				js_setup,
+				require("formatter.filetypes.javascript").eslint_d
+			)
+		end
+
+		if vim.fn.findfile(".prettierrc") ~= nil
+			or vim.fn.findfile(".prettierrc.json") ~= nil
+			or vim.fn.findfile(".prettierrc.js") ~= nil
+			or vim.fn.findfile("prettier.config.js") ~= nil
+		then
+			table.insert(
+				js_setup,
+				require("formatter.filetypes.javascript").prettierd
+			)
+		end
 
 		formatter.setup({
 			log_level = vim.log.levels.WARN,
 			filetype = {
-				-- javascript = { xo },
-				-- javascriptreact = { xo },
-				-- typescript = { xo },
-				-- typescriptreact = { xo },
-
-				javascript = eslintd_prettierd_setup,
-				javascriptreact = eslintd_prettierd_setup,
-				typescript = eslintd_prettierd_setup,
-				typescriptreact = eslintd_prettierd_setup,
-				vue = eslintd_prettierd_setup,
-				svelte = eslintd_prettierd_setup,
+				javascript = js_setup,
+				javascriptreact = js_setup,
+				typescript = js_setup,
+				typescriptreact = js_setup,
+				vue = js_setup,
+				svelte = js_setup,
 
 				dart = { require("formatter.filetypes.dart").dartformat },
 				lua = { require("formatter.filetypes.lua").stylua },
--- .config/nvim/lua/plugins/null-ls_nvim.lua
+++ .config/nvim/lua/plugins/null-ls_nvim.lua
@@ -1,17 +1,27 @@
 return {
 	"jose-elias-alvarez/null-ls.nvim",
 	config = function()
+		local sources = {
+			require("null-ls").builtins.diagnostics.fish,
+			require("null-ls").builtins.diagnostics.jsonlint,
+			require("null-ls").builtins.diagnostics.luacheck,
+			require("null-ls").builtins.diagnostics.yamllint,
+		}
+
+		if vim.fn.findfile(".xo-config.json") ~= nil then
+			table.insert(sources, require("null-ls").builtins.code_actions.xo)
+			table.insert(sources, require("null-ls").builtins.diagnostics.xo)
+		elseif
+			vim.fn.findfile(".eslintrc.js") ~= nil
+			or vim.fn.findfile(".eslintrc.json") ~= nil
+			or vim.fn.findfile(".eslintrc") ~= nil
+		then
+			table.insert(sources, require("null-ls").builtins.code_actions.eslint_d)
+			table.insert(sources, require("null-ls").builtins.diagnostics.eslint_d)
+		end
+
 		require("null-ls").setup({
-			sources = {
-				require("null-ls").builtins.code_actions.eslint_d,
-				-- require("null-ls").builtins.code_actions.xo,
-				require("null-ls").builtins.diagnostics.eslint_d,
-				require("null-ls").builtins.diagnostics.fish,
-				require("null-ls").builtins.diagnostics.jsonlint,
-				require("null-ls").builtins.diagnostics.luacheck,
-				require("null-ls").builtins.diagnostics.yamllint,
-				-- require("null-ls").builtins.diagnostics.xo,
-			},
+			sources = sources,
 		})
 	end,
 }
