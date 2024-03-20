471230bf8a12703910fa606f264533f7bb3f76a3
Aleksander Krawiel <a.krawiel@gmail.com>
2024-03-20 23:55:12 +0100

    Remove invalid code actions

--- .config/nvim/lua/plugins/null-ls_nvim.lua
+++ .config/nvim/lua/plugins/null-ls_nvim.lua
@@ -13,14 +13,6 @@ return {
 			}) > 0
 		end
 
-		if has_file(".xo-config.json") then
-			table.insert(sources, require("null-ls").builtins.code_actions.xo)
-			table.insert(sources, require("null-ls").builtins.diagnostics.xo)
-		elseif has_file(".eslintrc.js", ".eslintrc.json", ".eslintrc") then
-			table.insert(sources, require("null-ls").builtins.code_actions.eslint_d)
-			table.insert(sources, require("null-ls").builtins.diagnostics.eslint_d)
-		end
-
 		require("null-ls").setup({
 			sources = sources,
 		})
