5165b69d6f1ae1697b216cab4ebdead954b917f4
Aleksander Krawiel <a.krawiel@gmail.com>
2024-07-16 20:21:37 +0200

    Disable awesome window minimizing

--- .config/awesome/config/special_rules.lua
+++ .config/awesome/config/special_rules.lua
@@ -2,9 +2,14 @@ local awful = require("awful")
 local gears = require("gears")
 
 -- Special window treatment
+--
 client.connect_signal("property::class", function(c)
   -- Spotify empty boot class
 	if c.class == "Spotify" then
 		c:move_to_tag(awful.tag.find_by_name(nil, "9"))
 	end
+end)
+
+client.connect_signal("property::minimized", function(c)
+  c.minimized = false
 end)
