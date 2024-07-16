65e259881754899b2329b2e79256e38323c5ed99
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2024-07-16 18:06:39 +0200

    Add manual text yank highlight

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -23,3 +23,14 @@ local autocommands = {
 for _, autocmd in pairs(autocommands) do
 	vim.api.nvim_create_autocmd(unpack(autocmd))
 end
+
+local highlight_yank_group = vim.api.nvim_create_augroup("HighlightYank", {
+	clear = true,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 100 })
+	end,
+	group = highlight_yank_group,
+})
