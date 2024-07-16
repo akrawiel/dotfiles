fed2252c6e9210048c261d75b7d2afd9f8380639
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2024-07-16 18:06:01 +0200

    Remove unused plugins

--- .config/nvim/lua/plugins/fidget.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"j-hui/fidget.nvim",
-	opts = {
-		notification = {
-			override_vim_notify = true
-		}
-	}
-}
--- .config/nvim/lua/plugins/vim-highlightedyank.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"machakann/vim-highlightedyank",
-	config = function()
-		vim.g.highlightedyank_highlight_duration = 100
-	end,
-}
