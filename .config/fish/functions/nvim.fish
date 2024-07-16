affe088681551343af8ad3b4e00df84e5413e2cd
Aleksander Krawiel <a.krawiel@gmail.com>
2024-07-16 20:21:13 +0200

    Remove nvim function

--- .config/fish/functions/nvim.fish
+++
@@ -1,3 +0,0 @@
-function nvim --wraps='/usr/bin/nvim --listen /tmp/nvimsocket' --description 'alias nvim /usr/bin/nvim --listen /tmp/nvimsocket'
-  /usr/bin/nvim --listen /tmp/nvimsocket $argv; 
-end
