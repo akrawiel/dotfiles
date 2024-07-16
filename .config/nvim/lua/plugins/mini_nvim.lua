bec417acd0ab6639f53140ce6bdcf59a4d66b474
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2024-07-16 18:06:32 +0200

    Use mini nvim notify

--- .config/nvim/lua/plugins/mini_nvim.lua
+++ .config/nvim/lua/plugins/mini_nvim.lua
@@ -2,5 +2,18 @@ return {
 	"echasnovski/mini.nvim",
 	config = function()
 		require("mini.jump").setup({})
+
+		local notify = require("mini.notify")
+		notify.setup({
+			window = {
+				max_width_share = 1,
+				config = {
+					anchor = "SE",
+					border = "rounded",
+				},
+			},
+		})
+
+		vim.notify = notify.make_notify()
 	end,
 }
