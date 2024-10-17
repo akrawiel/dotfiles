95e88254f06a7d2b4ef7f6bcf9d5b4f4e304045a
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2024-10-17 10:02:43 +0200

    feat: change default apps booting

--- Scripts/boot-default-apps.fish
+++ Scripts/boot-default-apps.fish
@@ -1,11 +1,12 @@
 #!/usr/bin/env fish
 
 runSingleInstance 'kitty -c ~/.config/kitty/kitty.notes.conf --class xpad env NVIM_APPNAME=nvim.notes nvim ~/.config/nvim.notes.data/notes.md' 'xpad'
-#runSingleInstance 'xpad -s -N' 'xpad'
 
-runSingleInstance 'firefox'
-runSingleInstance 'slack -s'
-runSingleInstance '/var/lib/flatpak/exports/bin/com.slack.Slack' 'slack'
-runSingleInstance 'thunderbird'
-runSingleInstance '/var/lib/flatpak/exports/bin/org.ferdium.Ferdium' 'ferdium'
-runSingleInstance 'ferdium'
+runSingleInstance 'keepassxc' 'keepassxc' 'exact'
+runSingleInstance 'snap run spotify' 'spotify' 'flatpak'
+runSingleInstance 'firefox' 'firefox'
+runSingleInstance 'slack -s' 'slack'
+runSingleInstance 'flatpak run com.slack.Slack' 'slack'
+runSingleInstance 'thunderbird' 'thunderbird'
+runSingleInstance 'flatpak run org.ferdium.Ferdium' 'ferdium'
+runSingleInstance 'ferdium' 'ferdium'
