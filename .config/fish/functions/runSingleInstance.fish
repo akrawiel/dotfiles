8c3e25d6a5f903b4a662f181ebdf9e4429c6cba8
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2024-10-17 10:02:43 +0200

    Change default apps booting

--- .config/fish/functions/runSingleInstance.fish
+++ .config/fish/functions/runSingleInstance.fish
@@ -1,7 +1,7 @@
 function runSingleInstance
   set -l argCount (count $argv)
 
-  if test $argCount -eq 2
+  if test $argCount -eq 2 -o 3
     set -f condition $argv[2]
   else if test $argCount -eq 1
     set -f condition $argv[1]
@@ -9,7 +9,16 @@ function runSingleInstance
     return 1
   end
 
-  set -l result (pgrep -f "$condition")
+  if test $argCount -eq 3
+    if test $argv[3] = "exact"
+      set -f result (pgrep -x "$condition")
+    else
+      return 1
+    end
+  else
+    set -f result (pgrep -f "$condition")
+  end
+
   if test -z "$result"
     eval "$argv[1]&"
   end
