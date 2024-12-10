84a17ed759a313bc37ae249ca8764602fbfa58a6
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2024-12-10 15:26:11 +0100

    Clean up boot default apps

--- Scripts/boot-default-apps.fish
+++ Scripts/boot-default-apps.fish
@@ -1,12 +1,12 @@
 #!/usr/bin/env fish
 
-runSingleInstance 'kitty -c ~/.config/kitty/kitty.notes.conf --class xpad env NVIM_APPNAME=nvim.notes nvim ~/.config/nvim.notes.data/notes.md' 'xpad'
+runSingleInstance 'kitty -c ~/.config/kitty/kitty.notes.conf --class xpad env NVIM_APPNAME=nvim.notes nvim --cmd "cd ~/.config/nvim.notes.data" ~/.config/nvim.notes.data/notes.md' 'xpad'
 
 runSingleInstance 'keepassxc' 'keepassxc' 'exact'
-runSingleInstance 'snap run spotify' 'spotify' 'flatpak'
+runSingleInstance 'snap run spotify' 'spotify'
 runSingleInstance 'firefox' 'firefox'
 runSingleInstance 'slack -s' 'slack'
 runSingleInstance 'flatpak run com.slack.Slack' 'slack'
 runSingleInstance 'thunderbird' 'thunderbird'
-runSingleInstance 'flatpak run org.ferdium.Ferdium' 'ferdium'
 runSingleInstance 'ferdium' 'ferdium'
+runSingleInstance 'flatpak run org.ferdium.Ferdium' 'ferdium'
