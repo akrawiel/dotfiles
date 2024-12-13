69e42df9e99b8582fe23aeadab6b62db2f4d9ad9
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2024-12-13 16:26:12 +0100

    Change nvim notes to use gruvbox light soft

--- .config/nvim.notes/lua/plugins.lua
+++ .config/nvim.notes/lua/plugins.lua
@@ -1,17 +1,5 @@
 return {
 	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catppuccin",
-		priority = 1000,
-		opts = {
-			flavour = "latte",
-		},
-		config = function()
-			vim.cmd([[colorscheme catppuccin-latte]])
-		end,
-	},
-	{
 		"echasnovski/mini.nvim",
 		lazy = false,
 		config = function()
@@ -32,12 +20,57 @@ return {
 			require("mini.notify").setup()
 			require("mini.icons").setup()
 			require("mini.statusline").setup()
+
+			require("mini.base16").setup({
+				palette = {
+					["base00"] = "#f2e5bc",
+					["base01"] = "#ebdbb2",
+					["base02"] = "#d5c4a1",
+					["base03"] = "#bdae93",
+					["base04"] = "#665c54",
+					["base05"] = "#504945",
+					["base06"] = "#3c3836",
+					["base07"] = "#282828",
+					["base08"] = "#9d0006",
+					["base09"] = "#af3a03",
+					["base0A"] = "#b57614",
+					["base0B"] = "#79740e",
+					["base0C"] = "#427b58",
+					["base0D"] = "#076678",
+					["base0E"] = "#8f3f71",
+					["base0F"] = "#d65d0e",
+				},
+				plugins = { default = true },
+			})
 		end,
+	},
+	{
+		"rlane/pounce.nvim",
+		keys = {
+			{
+				"s",
+				mode = { "n", "v" },
+				function()
+					require("pounce").pounce(nil)
+				end,
+			},
+			{
+				"z",
+				mode = { "o" },
+				function()
+					require("pounce").pounce(nil)
+				end,
+			},
+		},
+	},
+	{
+		"stevearc/oil.nvim",
+		opts = {},
 		keys = {
 			{
 				"-",
 				function()
-					require("mini.files").open()
+					require("oil").open(nil)
 				end,
 			},
 		},
